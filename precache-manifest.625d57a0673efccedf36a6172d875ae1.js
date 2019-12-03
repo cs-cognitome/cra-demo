931e5373bfbbbd2a2c41772f76cc5e29f53f4435
cs-cognitome <gaya.scienza.v@gmail.com>
2019-12-03 10:11:26 +0200

    Updates

---
+++ precache-manifest.625d57a0673efccedf36a6172d875ae1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf531624e84fdbcd0ea8b905d97a1a75",
+    "url": "/cra-demo/index.html"
+  },
+  {
+    "revision": "b43519a5e67d41008fb0",
+    "url": "/cra-demo/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "6ea624aa9b9697035ed0",
+    "url": "/cra-demo/static/js/2.a3e8d21e.chunk.js"
+  },
+  {
+    "revision": "b43519a5e67d41008fb0",
+    "url": "/cra-demo/static/js/main.4494f7fc.chunk.js"
+  },
+  {
+    "revision": "51dd5d735150a0d15256",
+    "url": "/cra-demo/static/js/runtime-main.30833128.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cra-demo/precache-manifest.b74812e9ab6c5a3778565dc17ee09c9e.js"
+  "/cra-demo/precache-manifest.625d57a0673efccedf36a6172d875ae1.js"
 );
 
 self.addEventListener('message', (event) => {
