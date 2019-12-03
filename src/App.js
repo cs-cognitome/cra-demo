5792dd94ae1e92f5c89ff197dd1cbb4accf7bf21
cs-cognitome <gaya.scienza.v@gmail.com>
2019-12-03 10:15:21 +0200

    hello

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React from 'react';
 function App() {
   return (
       <div className="App">
-        <h1>Hello world!</h1>
+        <h1>Hello from Tanya Voronkova!</h1>
       </div>
     );
 }
